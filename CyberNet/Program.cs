73eebf5c55e6ddac93dd0e5e47ca2a02ef9722cb
Sergey Yakovlev <tac1402@gmail.com>
2013-04-22 03:19:31 +0300

    Начальный тест сервера

---
+++ CyberNet/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace CyberNet
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Запуск сервера...");
+			TcpListener Srv = new TcpListener(IPAddress.Any, 3148);
+			try
+			{
+				Srv.Start();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Не удалось запустить сервер: {0}", e.Message);
+				Console.ReadKey();
+				return;
+			}
+
+			Console.WriteLine("Сервер запущен. Ожидание подключения...");
+
+			TcpClient Client = Srv.AcceptTcpClient();
+
+			NetworkStream ClientStream = Client.GetStream();
+
+			Console.WriteLine("Входящее соединение ({0})", Client.Client.RemoteEndPoint.ToString());
+
+			string response = "Ответ сервера клиенту";
+			byte[] responseBuffer = Encoding.Default.GetBytes(response);
+			ClientStream.Write(responseBuffer, 0, responseBuffer.Length);
+
+			ClientStream.Close();
+			Console.WriteLine("Ответ клиенту отправлен. Работа завершена.");
+			Console.WriteLine("Нажмите любую клавишу...");
+			Console.ReadKey();	
+		
+		}
+	}
+}
---
+++ CyberNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("CyberNet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CyberNet")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f48f8858-9791-4f74-980c-23dc94b33471")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
